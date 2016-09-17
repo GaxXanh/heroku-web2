2075089305f46b3243bbafcd58ff85b990214e30
linhnt2803 <linhnt28031995@gmail.com>
2016-09-17 21:14:34 +0700

    test

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.static(__dirname + '/public'))
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
-
+app.get('/a', function(req, res) {
+  res.end('a');
+})
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
